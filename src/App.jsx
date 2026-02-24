34c481c4d1f92f254f3192977b51e681a734bc06
kwan ho lam <kwanho.app@gmail.com>
2026-02-25 00:33:07 +0800

    chore: try my-self

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Task1_1_JSX from './task_1.1_JSX'
+import Task1_1_Play from './task_1.1_play'
 
 function App() {
     return (
         <div className="App">
             <Task1_1_JSX />
+            <Task1_1_Play />
         </div>
     )
 }
---
+++ src/task_1.1_play.jsx
@@ -0,0 +1,11 @@
+function Task1_1_Play() {
+    return (
+        <div>
+            <h1>Task 1.1 Play</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, aperiam? Nesciunt ipsam et aliquid beatae voluptates quisquam magni voluptas aperiam. Nulla dolor ex, soluta ab animi inventore placeat eaque sit?</p>
+            <button>I am button</button>
+        </div>
+    )
+}
+
+export default Task1_1_Play
