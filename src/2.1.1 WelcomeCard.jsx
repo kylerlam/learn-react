dd8299add046c25753d27809bc248bcffe5311e2
kwan ho lam <kwanho.app@gmail.com>
2026-02-25 19:08:03 +0800

    chore: add sub component change data of main component

--- src/2.1.1 WelcomeCard.jsx
+++ src/2.1.1 WelcomeCard.jsx
@@ -1,11 +1,35 @@
 
-function WelcomeCard(props) { // 上面用props，下面用props.name...：不解構 
+function WelcomeCard({ name, place, number, onNameChange, onPlaceChange, onNumberChange }) { // 上面用props，下面用props.name...：不解構 
     return (
         <div className="p-4 border rounded shadow">
             {/* 使用 props.屬性名稱 來取得內容 */}
-            <h1>你好, {props.name || "原始人沒有電話號碼"}！</h1>
-            <p>歡迎來到 {props.place || "原始人沒有電話號碼"}</p>
-            <p>電話: {props.number || "原始人沒有電話號碼"}</p>
+            <h1>你好, {name}！</h1>
+            <p>歡迎來到 {place}</p>
+            <p>電話: {number}</p>
+            <div>
+                <label className="text-sm text-gray-500">在子組件修改名字：</label>
+                <input
+                    type="text"
+                    className="border p-1 rounded w-full"
+                    placeholder="輸入新名字..."
+                    value={name} // 依然顯示從父組件傳來的數據
+                    onChange={(e) => onNameChange(e.target.value)} // ！！！關鍵：通知父組件變更資料
+                />
+                <input
+                    type="text"
+                    className="border p-1 rounded w-full"
+                    placeholder="輸入新地點..."
+                    value={place} // 依然顯示從父組件傳來的數據
+                    onChange={(e) => onPlaceChange(e.target.value)} // ！！！關鍵：通知父組件變更資料
+                />
+                <input
+                    type="text"
+                    className="border p-1 rounded w-full"
+                    placeholder="輸入新號碼..."
+                    value={number} // 依然顯示從父組件傳來的數據
+                    onChange={(e) => onNumberChange(e.target.value)} // ！！！關鍵：通知父組件變更資料
+                />
+            </div>
         </div>
     );
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Task1_1_JSX from './1.1.1 JSX'
 import Task1_1_Play from './1.1.2 Play'
 import Task1_1_DataStore from './1.1.3 DataStore'
@@ -6,6 +7,10 @@ import Task1_1_Addition from './1.1.5 Addition'
 import WelcomeCard from './2.1.1 WelcomeCard'
 
 function App() {
+    const [name, setName] = useState("小美");
+    const [place, setPlace] = useState("Taipei");
+    const [number, setNumber] = useState("12312312");
+
     return (
         <div className="App">
             <Task1_1_JSX />
@@ -13,9 +18,14 @@ function App() {
             <Task1_1_DataStore />
             <Task1_1_ChangeColor />
             <Task1_1_Addition />
-            <WelcomeCard name="小美" place="Taipei" number="12312312" />
-            <WelcomeCard name="小明" place="Hong Kong" />
-            <WelcomeCard name="小華" place="Shanghai" />
+            <WelcomeCard
+                name={name}
+                place={place}
+                number={number}
+                onNameChange={setName}
+                onPlaceChange={setPlace}
+                onNumberChange={setNumber}
+            />
         </div>
     )
 }
