0ca4916ccf6a8682f65b576874669257a527ceba
kwan ho lam <kwanho.app@gmail.com>
2026-02-28 18:42:50 +0800

    feat: add addition in TodoList

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -3,9 +3,25 @@ import { useState } from "react"
 
 function TodoApp() {
     const [todos, setTodos] = useState(["買牛奶", "倒垃圾"])
+    const [inputValue, setInputValue] = useState("")
+
+    function handleAddTodo() {
+        if (inputValue.trim() === "") return
+        setTodos([...todos, inputValue]) // 後面的新項目加入前面的舊項目 ["買牛奶", "倒垃圾"]
+        setInputValue("")
+    }
     return (
         <div>
             <h1>我的待辦事項</h1>
+
+
+            <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                placeholder="輸入新的待辦事項"
+            />
+            <button onClick={handleAddTodo}>新增</button>
             {
                 todos.map((todo, index) => (
                     <li key={index}>{todo}</li>
@@ -15,3 +31,8 @@ function TodoApp() {
     )
 }
 export default TodoApp
+
+
+// const [todos, setTodos] = useState(["買牛奶", "倒垃圾"])
+//         ↑        ↑
+//       讀數據    寫數據
