07f2666cfaf73cf5a170915a6d0e956c7913e1bb
kwan ho lam <kwanho.app@gmail.com>
2026-02-28 19:07:26 +0800

    feat: add deletion

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -2,31 +2,38 @@ import { useState } from "react"
 
 
 function TodoApp() {
-    const [todos, setTodos] = useState(["買牛奶", "倒垃圾"])
+    const [todos, setTodos] = useState([])
     const [inputValue, setInputValue] = useState("")
 
     function handleAddTodo() {
         if (inputValue.trim() === "") return
-        setTodos([...todos, inputValue]) // 後面的新項目加入前面的舊項目 ["買牛奶", "倒垃圾"]
+        setTodos([...todos, inputValue]) // 後面的新項目加入前面的舊項目
         setInputValue("")
+    }
+    function handleDeleteTodo(index) {
+        setTodos(todos.filter((_, i) => i !== index))
     }
     return (
         <div>
-            <h1>我的待辦事項</h1>
+            <h1>我的待辦事項：</h1>
 
-
+            {
+                todos.map((todo, index) => (
+                    <li key={index}>
+                        {todo}
+                        <button onClick={() => handleDeleteTodo(index)}> ✗ </button>
+                    </li>
+                ))
+            }
+            <br></br>
             <input
                 type="text"
                 value={inputValue}
                 onChange={(e) => setInputValue(e.target.value)}
                 placeholder="輸入新的待辦事項"
-            />
-            <button onClick={handleAddTodo}>新增</button>
-            {
-                todos.map((todo, index) => (
-                    <li key={index}>{todo}</li>
-                ))
-            }
+            /><br></br>
+            <button onClick={handleAddTodo}>新增&#160;</button>
+
         </div>
     )
 }
